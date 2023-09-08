66d6673755022fe3c3cb8fa46975a5164cf1f3e7
Alice Butcher <alycejenni@gmail.com>
2023-09-08 12:33:29 +0100

    feat: add very basic vite/vue app

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <zoa-flash header="Greetings!" style="margin: 1em"></zoa-flash>
+</template>
+
+<script setup>
+import { ZoaFlash } from '@nhm-data/zoa';
+</script>
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import '@nhm-data/zoa/theme';
+
+const app = createApp(App);
+app.mount('#app');

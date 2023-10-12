e5911903ea8e7a60d36ffa491765f903209daddf
Alice Butcher <alycejenni@gmail.com>
2023-10-12 16:59:48 +0100

    feat: add pinia and pinia-orm

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import { createORM } from 'pinia-orm';
 import App from './App.vue';
 import '@nhm-data/zoa/theme';
 
+const pinia = createPinia().use(createORM());
+
 const app = createApp(App);
+app.use(pinia);
 app.mount('#app');
